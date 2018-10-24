c28333112b7dbe5b20cee000e1c985bae8d5885e
Romaric Lalaina <romariiick@gmail.com>
2018-10-24 17:11:24 +0200

    Add import os and regex

--- datastructure/datastructure.py
+++ datastructure/datastructure.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 list = ["a", "b", "c", "d"]
 
 list.append(33)
@@ -7,5 +10,10 @@ print list
 list.pop(0)
 
 print list
-
 print list.index('c')
+
+print os.getcwd()
+
+print dir(os)
+
+print re.findall(r'\bf[a-z]*', 'which foot or hand fell fastest')
---
+++ inputoutput/inputouput.py
@@ -0,0 +1,15 @@
+
+import math
+import json
+
+s = "Hello, word"
+
+print math.pi
+
+print s.rjust(3)
+
+print json.dumps([1, 'simple', 'liste'])
+
+
+
+
