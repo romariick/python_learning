73970c5a93bb1c8280e932b8862ef15b7c597f3c
Romaric Lalaina <romariiick@gmail.com>
2018-10-26 13:59:30 +0200

    Add functionnal programming tools

---
+++ functionnal_programming/functionnal_programming.py
@@ -0,0 +1,18 @@
+
+def f(x):
+    return x % 3 == 0 or x % 5 == 0
+
+#x = filter(f, range(1, 1000))
+#x = map(f, range(1, 10))
+
+#print x
+
+#[(x, y) for i in [1, 2, 3] for y in [2, 5, 6, 8] if x != y]
+
+#print (x, y)
+
+vec = [1, 2, 6]
+
+[x * 2 for x in vec]
+
+print x
